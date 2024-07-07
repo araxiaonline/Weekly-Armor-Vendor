81befd3d60c32078478b5b63a355dc68f2e1887e
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2024-07-07 13:24:53 -0400

    Create black_market_armor_set_Table_Creation.sql

---
+++ black_market_armor_set_Table_Creation.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `black_market_armor_sets` (
+    `class` INT NOT NULL,
+    `item_id` INT NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `item_set_id` INT NOT NULL,
+    `set_name` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`item_id`)
+);
