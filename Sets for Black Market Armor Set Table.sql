3e322620d69c2802f3b7cca9040910061d3be509
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2024-07-08 21:55:29 -0400

    Update Sets for Black Market Armor Set Table.sql
    Added up to Dungeon Sets 2

--- Sets for Black Market Armor Set Table.sql
+++ Sets for Black Market Armor Set Table.sql
@@ -1,5 +1,6 @@
 -- Insert Armor Sets
 
+
 --[[	Lieutenant Commander's SETS]]
 
 
@@ -274,3 +275,292 @@ INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`
 (1, 16483, 'Marshal''s Plate Boots', 384, 'Field Marshal''s Battlegear'),
 (1, 16484, 'Marshal''s Plate Gauntlets', 384, 'Field Marshal''s Battlegear'),
 (1, 16479, 'Marshal''s Plate Legguards', 384, 'Field Marshal''s Battlegear');
+
+-- Insert Armor Sets for Druid: Warlord's Sanctuary Item Set 398
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(11, 16554, 'General''s Dragonhide Boots', 398, 'Warlord''s Sanctuary'),
+(11, 16555, 'General''s Dragonhide Gloves', 398, 'Warlord''s Sanctuary'),
+(11, 16552, 'General''s Dragonhide Leggings', 398, 'Warlord''s Sanctuary'),
+(11, 16551, 'Warlord''s Dragonhide Epaulets', 398, 'Warlord''s Sanctuary'),
+(11, 16549, 'Warlord''s Dragonhide Hauberk', 398, 'Warlord''s Sanctuary'),
+(11, 16550, 'Warlord''s Dragonhide Helmet', 398, 'Warlord''s Sanctuary');
+
+-- Insert Armor Sets for Hunter: Warlord's Pursuit Item Set 396
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(3, 16569, 'General''s Chain Boots', 396, 'Warlord''s Pursuit'),
+(3, 16571, 'General''s Chain Gloves', 396, 'Warlord''s Pursuit'),
+(3, 16567, 'General''s Chain Legguards', 396, 'Warlord''s Pursuit'),
+(3, 16565, 'Warlord''s Chain Chestpiece', 396, 'Warlord''s Pursuit'),
+(3, 16566, 'Warlord''s Chain Helmet', 396, 'Warlord''s Pursuit'),
+(3, 16568, 'Warlord''s Chain Shoulders', 396, 'Warlord''s Pursuit');
+
+-- Insert Armor Sets for Mage: Warlord's Regalia Item Set 387
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(8, 16536, 'Warlord''s Silk Amice', 387, 'Warlord''s Regalia'),
+(8, 16533, 'Warlord''s Silk Cowl', 387, 'Warlord''s Regalia'),
+(8, 16535, 'Warlord''s Silk Raiment', 387, 'Warlord''s Regalia'),
+(8, 16539, 'General''s Silk Boots', 387, 'Warlord''s Regalia'),
+(8, 16540, 'General''s Silk Handguards', 387, 'Warlord''s Regalia'),
+(8, 16534, 'General''s Silk Trousers', 387, 'Warlord''s Regalia');
+
+-- Insert Armor Sets for Paladin: Warlord's Aegis Item Set 698
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(2, 29612, 'General''s Lamellar Boots', 698, 'Warlord''s Aegis'),
+(2, 29613, 'General''s Lamellar Gloves', 698, 'Warlord''s Aegis'),
+(2, 29614, 'General''s Lamellar Legplates', 698, 'Warlord''s Aegis'),
+(2, 29615, 'Warlord''s Lamellar Chestplate', 698, 'Warlord''s Aegis'),
+(2, 29616, 'Warlord''s Lamellar Faceguard', 698, 'Warlord''s Aegis'),
+(2, 29617, 'Warlord''s Lamellar Pauldrons', 698, 'Warlord''s Aegis');
+
+-- Insert Armor Sets for Priest: Warlord's Raiment Item Set 390
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(5, 17623, 'Warlord''s Satin Cowl', 390, 'Warlord''s Raiment'),
+(5, 17625, 'General''s Satin Leggings', 390, 'Warlord''s Raiment'),
+(5, 17622, 'Warlord''s Satin Mantle', 390, 'Warlord''s Raiment'),
+(5, 17624, 'Warlord''s Satin Robes', 390, 'Warlord''s Raiment'),
+(5, 17618, 'General''s Satin Boots', 390, 'Warlord''s Raiment'),
+(5, 17620, 'General''s Satin Gloves', 390, 'Warlord''s Raiment');
+
+-- Insert Armor Sets for Rogue: Warlord's Vestments Item Set 393
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(4, 16563, 'Warlord''s Leather Breastplate', 393, 'Warlord''s Vestments'),
+(4, 16561, 'Warlord''s Leather Helm', 393, 'Warlord''s Vestments'),
+(4, 16562, 'Warlord''s Leather Spaulders', 393, 'Warlord''s Vestments'),
+(4, 16564, 'General''s Leather Legguards', 393, 'Warlord''s Vestments'),
+(4, 16560, 'General''s Leather Mitts', 393, 'Warlord''s Vestments'),
+(4, 16558, 'General''s Leather Treads', 393, 'Warlord''s Vestments');
+
+-- Insert Armor Sets for Shaman: Warlord's Earthshaker Item Set 386
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(7, 16577, 'Warlord''s Mail Armor', 386, 'Warlord''s Earthshaker'),
+(7, 16578, 'Warlord''s Mail Helm', 386, 'Warlord''s Earthshaker'),
+(7, 16580, 'Warlord''s Mail Spaulders', 386, 'Warlord''s Earthshaker'),
+(7, 16573, 'General''s Mail Boots', 386, 'Warlord''s Earthshaker'),
+(7, 16574, 'General''s Mail Gauntlets', 386, 'Warlord''s Earthshaker'),
+(7, 16579, 'General''s Mail Leggings', 386, 'Warlord''s Earthshaker');
+
+-- Insert Armor Sets for Warlock: Warlord's Threads Item Set 391
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(9, 17586, 'General''s Dreadweave Boots', 391, 'Warlord''s Threads'),
+(9, 17588, 'General''s Dreadweave Gloves', 391, 'Warlord''s Threads'),
+(9, 17593, 'General''s Dreadweave Pants', 391, 'Warlord''s Threads'),
+(9, 17591, 'Warlord''s Dreadweave Hood', 391, 'Warlord''s Threads'),
+(9, 17590, 'Warlord''s Dreadweave Mantle', 391, 'Warlord''s Threads'),
+(9, 17592, 'Warlord''s Dreadweave Robe', 391, 'Warlord''s Threads');
+
+-- Insert Armor Sets for Warrior: Warlord's Battlegear Item Set 383
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(1, 16541, 'Warlord''s Plate Armor', 383, 'Warlord''s Battlegear'),
+(1, 16542, 'Warlord''s Plate Headpiece', 383, 'Warlord''s Battlegear'),
+(1, 16544, 'Warlord''s Plate Shoulders', 383, 'Warlord''s Battlegear'),
+(1, 16545, 'General''s Plate Boots', 383, 'Warlord''s Battlegear'),
+(1, 16548, 'General''s Plate Gauntlets', 383, 'Warlord''s Battlegear'),
+(1, 16543, 'General''s Plate Leggings', 383, 'Warlord''s Battlegear');
+
+
+--[[ Dungeon Set 1 ]]
+
+
+
+-- Insert Armor Sets for Druid: Wildheart Raiment Item Set 185
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(11, 16716, 'Wildheart Belt', 185, 'Wildheart Raiment'),
+(11, 16715, 'Wildheart Boots', 185, 'Wildheart Raiment'),
+(11, 16714, 'Wildheart Bracers', 185, 'Wildheart Raiment'),
+(11, 16720, 'Wildheart Cowl', 185, 'Wildheart Raiment'),
+(11, 16706, 'Wildheart Vest', 185, 'Wildheart Raiment'),
+(11, 16718, 'Wildheart Spaulders', 185, 'Wildheart Raiment'),
+(11, 16719, 'Wildheart Kilt', 185, 'Wildheart Raiment'),
+(11, 16717, 'Wildheart Gloves', 185, 'Wildheart Raiment');
+
+-- Insert Armor Sets for Hunter: Beaststalker Armor Item Set 186
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(3, 16680, 'Beaststalker''s Belt', 186, 'Beaststalker Armor'),
+(3, 16675, 'Beaststalker''s Boots', 186, 'Beaststalker Armor'),
+(3, 16681, 'Beaststalker''s Bindings', 186, 'Beaststalker Armor'),
+(3, 16677, 'Beaststalker''s Cap', 186, 'Beaststalker Armor'),
+(3, 16674, 'Beaststalker''s Tunic', 186, 'Beaststalker Armor'),
+(3, 16678, 'Beaststalker''s Pants', 186, 'Beaststalker Armor'),
+(3, 16679, 'Beaststalker''s Mantle', 186, 'Beaststalker Armor'),
+(3, 16676, 'Beaststalker''s Gloves', 186, 'Beaststalker Armor');
+
+-- Insert Armor Sets for Mage: Magister's Regalia Item Set 181
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(8, 16685, 'Magister''s Belt', 181, 'Magister''s Regalia'),
+(8, 16683, 'Magister''s Bindings', 181, 'Magister''s Regalia'),
+(8, 16686, 'Magister''s Crown', 181, 'Magister''s Regalia'),
+(8, 16684, 'Magister''s Gloves', 181, 'Magister''s Regalia'),
+(8, 16687, 'Magister''s Leggings', 181, 'Magister''s Regalia'),
+(8, 16689, 'Magister''s Mantle', 181, 'Magister''s Regalia'),
+(8, 16688, 'Magister''s Robes', 181, 'Magister''s Regalia'),
+(8, 16682, 'Magister''s Boots', 181, 'Magister''s Regalia');
+
+-- Insert Armor Sets for Paladin: Lightforge Armor Item Set 188
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(2, 16723, 'Lightforge Belt', 188, 'Lightforge Armor'),
+(2, 16725, 'Lightforge Boots', 188, 'Lightforge Armor'),
+(2, 16722, 'Lightforge Bracers', 188, 'Lightforge Armor'),
+(2, 16726, 'Lightforge Breastplate', 188, 'Lightforge Armor'),
+(2, 16724, 'Lightforge Gauntlets', 188, 'Lightforge Armor'),
+(2, 16728, 'Lightforge Legplates', 188, 'Lightforge Armor'),
+(2, 16729, 'Lightforge Spaulders', 188, 'Lightforge Armor'),
+(2, 16727, 'Lightforge Helm', 188, 'Lightforge Armor');
+
+-- Insert Armor Sets for Priest: Vestments of the Devout Item Set 182
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(5, 16696, 'Devout Belt', 182, 'Vestments of the Devout'),
+(5, 16691, 'Devout Sandals', 182, 'Vestments of the Devout'),
+(5, 16697, 'Devout Bracers', 182, 'Vestments of the Devout'),
+(5, 16693, 'Devout Crown', 182, 'Vestments of the Devout'),
+(5, 16692, 'Devout Gloves', 182, 'Vestments of the Devout'),
+(5, 16695, 'Devout Mantle', 182, 'Vestments of the Devout'),
+(5, 16694, 'Devout Skirt', 182, 'Vestments of the Devout'),
+(5, 16690, 'Devout Robe', 182, 'Vestments of the Devout');
+
+-- Insert Armor Sets for Rogue: Shadowcraft Armor Item Set 184
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(4, 16713, 'Shadowcraft Belt', 184, 'Shadowcraft Armor'),
+(4, 16711, 'Shadowcraft Boots', 184, 'Shadowcraft Armor'),
+(4, 16710, 'Shadowcraft Bracers', 184, 'Shadowcraft Armor'),
+(4, 16721, 'Shadowcraft Tunic', 184, 'Shadowcraft Armor'),
+(4, 16708, 'Shadowcraft Spaulders', 184, 'Shadowcraft Armor'),
+(4, 16709, 'Shadowcraft Pants', 184, 'Shadowcraft Armor'),
+(4, 16712, 'Shadowcraft Gloves', 184, 'Shadowcraft Armor'),
+(4, 16707, 'Shadowcraft Cap', 184, 'Shadowcraft Armor');
+
+-- Insert Armor Sets for Shaman: The Elements Item Set 187
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(7, 16673, 'Cord of Elements', 187, 'The Elements'),
+(7, 16670, 'Boots of Elements', 187, 'The Elements'),
+(7, 16671, 'Bindings of Elements', 187, 'The Elements'),
+(7, 16667, 'Coif of Elements', 187, 'The Elements'),
+(7, 16672, 'Gauntlets of Elements', 187, 'The Elements'),
+(7, 16668, 'Kilt of Elements', 187, 'The Elements'),
+(7, 16669, 'Pauldrons of Elements', 187, 'The Elements'),
+(7, 16666, 'Vest of Elements', 187, 'The Elements');
+
+-- Insert Armor Sets for Warlock: Dreadmist Raiment Item Set 183
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(9, 16702, 'Dreadmist Belt', 183, 'Dreadmist Raiment'),
+(9, 16703, 'Dreadmist Bracers', 183, 'Dreadmist Raiment'),
+(9, 16699, 'Dreadmist Leggings', 183, 'Dreadmist Raiment'),
+(9, 16701, 'Dreadmist Mantle', 183, 'Dreadmist Raiment'),
+(9, 16700, 'Dreadmist Robe', 183, 'Dreadmist Raiment'),
+(9, 16704, 'Dreadmist Sandals', 183, 'Dreadmist Raiment'),
+(9, 16698, 'Dreadmist Mask', 183, 'Dreadmist Raiment'),
+(9, 16705, 'Dreadmist Wraps', 183, 'Dreadmist Raiment');
+
+-- Insert Armor Sets for Warrior: Battlegear of Valor Item Set 189
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(1, 16736, 'Belt of Valor', 189, 'Battlegear of Valor'),
+(1, 16734, 'Boots of Valor', 189, 'Battlegear of Valor'),
+(1, 16735, 'Bracers of Valor', 189, 'Battlegear of Valor'),
+(1, 16730, 'Breastplate of Valor', 189, 'Battlegear of Valor'),
+(1, 16737, 'Gauntlets of Valor', 189, 'Battlegear of Valor'),
+(1, 16731, 'Helm of Valor', 189, 'Battlegear of Valor'),
+(1, 16732, 'Legplates of Valor', 189, 'Battlegear of Valor'),
+(1, 16733, 'Spaulders of Valor', 189, 'Battlegear of Valor');
+
+
+--[[ Dungeon Set 2 ]]
+
+
+
+-- Insert Armor Sets for Druid: Feralheart Raiment Item Set 513
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(11, 22106, 'Feralheart Belt', 513, 'Feralheart Raiment'),
+(11, 22107, 'Feralheart Boots', 513, 'Feralheart Raiment'),
+(11, 22108, 'Feralheart Bracers', 513, 'Feralheart Raiment'),
+(11, 22109, 'Feralheart Cowl', 513, 'Feralheart Raiment'),
+(11, 22110, 'Feralheart Gloves', 513, 'Feralheart Raiment'),
+(11, 22111, 'Feralheart Kilt', 513, 'Feralheart Raiment'),
+(11, 22112, 'Feralheart Spaulders', 513, 'Feralheart Raiment'),
+(11, 22113, 'Feralheart Vest', 513, 'Feralheart Raiment');
+
+-- Insert Armor Sets for Hunter: Beastmaster Armor Item Set 515
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(3, 22010, 'Beastmaster\'s Belt', 515, 'Beastmaster Armor'),
+(3, 22011, 'Beastmaster\'s Bindings', 515, 'Beastmaster Armor'),
+(3, 22061, 'Beastmaster\'s Boots', 515, 'Beastmaster Armor'),
+(3, 22013, 'Beastmaster\'s Cap', 515, 'Beastmaster Armor'),
+(3, 22015, 'Beastmaster\'s Gloves', 515, 'Beastmaster Armor'),
+(3, 22016, 'Beastmaster\'s Mantle', 515, 'Beastmaster Armor'),
+(3, 22017, 'Beastmaster\'s Pants', 515, 'Beastmaster Armor'),
+(3, 22060, 'Beastmaster\'s Tunic', 515, 'Beastmaster Armor');
+
+-- Insert Armor Sets for Mage: Sorcerer's Regalia Item Set 517
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(8, 22062, 'Sorcerer\'s Belt', 517, 'Sorcerer\'s Regalia'),
+(8, 22063, 'Sorcerer\'s Bindings', 517, 'Sorcerer\'s Regalia'),
+(8, 22064, 'Sorcerer\'s Boots', 517, 'Sorcerer\'s Regalia'),
+(8, 22065, 'Sorcerer\'s Crown', 517, 'Sorcerer\'s Regalia'),
+(8, 22066, 'Sorcerer\'s Gloves', 517, 'Sorcerer\'s Regalia'),
+(8, 22067, 'Sorcerer\'s Leggings', 517, 'Sorcerer\'s Regalia'),
+(8, 22068, 'Sorcerer\'s Mantle', 517, 'Sorcerer\'s Regalia'),
+(8, 22069, 'Sorcerer\'s Robes', 517, 'Sorcerer\'s Regalia');
+
+-- Insert Armor Sets for Paladin: Soulforge Armor Item Set 516
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(2, 22086, 'Soulforge Belt', 516, 'Soulforge Armor'),
+(2, 22087, 'Soulforge Boots', 516, 'Soulforge Armor'),
+(2, 22088, 'Soulforge Bracers', 516, 'Soulforge Armor'),
+(2, 22089, 'Soulforge Breastplate', 516, 'Soulforge Armor'),
+(2, 22090, 'Soulforge Gauntlets', 516, 'Soulforge Armor'),
+(2, 22091, 'Soulforge Helm', 516, 'Soulforge Armor'),
+(2, 22092, 'Soulforge Legplates', 516, 'Soulforge Armor'),
+(2, 22093, 'Soulforge Spaulders', 516, 'Soulforge Armor');
+
+-- Insert Armor Sets for Priest: Vestments of the Virtuous Item Set 514
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(5, 22078, 'Virtuous Belt', 514, 'Vestments of the Virtuous'),
+(5, 22079, 'Virtuous Bracers', 514, 'Vestments of the Virtuous'),
+(5, 22080, 'Virtuous Crown', 514, 'Vestments of the Virtuous'),
+(5, 22081, 'Virtuous Gloves', 514, 'Vestments of the Virtuous'),
+(5, 22082, 'Virtuous Mantle', 514, 'Vestments of the Virtuous'),
+(5, 22083, 'Virtuous Robe', 514, 'Vestments of the Virtuous'),
+(5, 22084, 'Virtuous Sandals', 514, 'Vestments of the Virtuous'),
+(5, 22085, 'Virtuous Skirt', 514, 'Vestments of the Virtuous');
+
+-- Insert Armor Sets for Rogue: Darkmantle Armor Item Set 512
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(4, 22002, 'Darkmantle Belt', 512, 'Darkmantle Armor'),
+(4, 22003, 'Darkmantle Boots', 512, 'Darkmantle Armor'),
+(4, 22004, 'Darkmantle Bracers', 512, 'Darkmantle Armor'),
+(4, 22005, 'Darkmantle Cap', 512, 'Darkmantle Armor'),
+(4, 22006, 'Darkmantle Gloves', 512, 'Darkmantle Armor'),
+(4, 22007, 'Darkmantle Pants', 512, 'Darkmantle Armor'),
+(4, 22008, 'Darkmantle Spaulders', 512, 'Darkmantle Armor'),
+(4, 22009, 'Darkmantle Tunic', 512, 'Darkmantle Armor');
+
+-- Insert Armor Sets for Shaman: The Five Thunders Item Set 519
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(7, 22095, 'Bindings of The Five Thunders', 519, 'The Five Thunders'),
+(7, 22096, 'Boots of The Five Thunders', 519, 'The Five Thunders'),
+(7, 22097, 'Coif of The Five Thunders', 519, 'The Five Thunders'),
+(7, 22098, 'Cord of The Five Thunders', 519, 'The Five Thunders'),
+(7, 22099, 'Gauntlets of The Five Thunders', 519, 'The Five Thunders'),
+(7, 22100, 'Kilt of The Five Thunders', 519, 'The Five Thunders'),
+(7, 22101, 'Pauldrons of The Five Thunders', 519, 'The Five Thunders'),
+(7, 22102, 'Vest of The Five Thunders', 519, 'The Five Thunders');
+
+-- Insert Armor Sets for Warlock: Deathmist Raiment Item Set 518
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(9, 22070, 'Deathmist Belt', 518, 'Deathmist Raiment'),
+(9, 22071, 'Deathmist Bracers', 518, 'Deathmist Raiment'),
+(9, 22072, 'Deathmist Leggings', 518, 'Deathmist Raiment'),
+(9, 22073, 'Deathmist Mantle', 518, 'Deathmist Raiment'),
+(9, 22074, 'Deathmist Mask', 518, 'Deathmist Raiment'),
+(9, 22075, 'Deathmist Robe', 518, 'Deathmist Raiment'),
+(9, 22076, 'Deathmist Sandals', 518, 'Deathmist Raiment'),
+(9, 22077, 'Deathmist Wraps', 518, 'Deathmist Raiment');
+
+-- Insert Armor Sets for Warrior: Battlegear of Heroism Item Set 511
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(1, 21994, 'Belt of Heroism', 511, 'Battlegear of Heroism'),
+(1, 21995, 'Boots of Heroism', 511, 'Battlegear of Heroism'),
+(1, 21996, 'Bracers of Heroism', 511, 'Battlegear of Heroism'),
+(1, 21997, 'Breastplate of Heroism', 511, 'Battlegear of Heroism'),
+(1, 21998, 'Gauntlets of Heroism', 511, 'Battlegear of Heroism'),
+(1, 21999, 'Helm of Heroism', 511, 'Battlegear of Heroism'),
+(1, 22000, 'Legplates of Heroism', 511, 'Battlegear of Heroism'),
+(1, 22001, 'Spaulders of Heroism', 511, 'Battlegear of Heroism');
