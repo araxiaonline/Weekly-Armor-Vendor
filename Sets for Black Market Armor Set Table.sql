814abca7c17c181fe9f22257648ca01b45c56091
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2024-07-08 20:47:51 -0400

    Create Sets for Black Market Armor Set Table.sql

---
+++ Sets for Black Market Armor Set Table.sql
@@ -0,0 +1,283 @@
+-- Insert Armor Sets
+
+
+--[[	Lieutenant Commander's SETS]]
+
+
+--Druid: Lieutenant Commander's Refuge
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+
+(11,23294,'Knight-Captain''s Dragonhide Chestpiece',551,'Lieutenant Commander''s Refuge'),
+(11,23295,'Knight-Captain''s Dragonhide Leggings',551,'Lieutenant Commander''s Refuge'),
+(11,23280,'Knight-Lieutenant''s Dragonhide Grips',551,'Lieutenant Commander''s Refuge'),
+(11,23281,'Knight-Lieutenant''s Dragonhide Treads',551,'Lieutenant Commander''s Refuge'),
+(11,23308,'Lieutenant Commander''s Dragonhide Headguard',551,'Lieutenant Commander''s Refuge'),
+(11,23309,'Lieutenant Commander''s Dragonhide Shoulders',551,'Lieutenant Commander''s Refuge'),
+
+--Hunter: Lieutenant Commander's Pursuance
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+
+(3,16425,'Knight-Captain''s Chain Hauberk',550,'Lieutenant Commander''s Pursuance'),
+(3,16426,'Knight-Captain''s Chain Leggings',550,'Lieutenant Commander''s Pursuance'),
+(3,23279,'Knight-Lieutenant''s Chain Vices',550,'Lieutenant Commander''s Pursuance'),
+(3,16401,'Knight-Lieutenant''s Chain Boots',550,'Lieutenant Commander''s Pursuance');
+(3,23306,'Lieutenant Commander''s Chain Helm',550,'Lieutenant Commander''s Pursuance'),
+(3,23307,'Lieutenant Commander''s Chain Shoulders',550,'Lieutenant Commander''s Pursuance'),
+
+--Mage: Lieutenant Commander's Arcanum
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+
+(8,16413,'Knight-Captain''s Silk Tunic',546,'Lieutenant Commander''s Arcanum'),
+(8,16414,'Knight-Captain''s Silk Legguards',546,'Lieutenant Commander''s Arcanum'),
+(8,23290,'Knight-Lieutenant''s Silk Handwraps',546,'Lieutenant Commander''s Arcanum'),
+(8,23291,'Knight-Lieutenant''s Silk Walkers',546,'Lieutenant Commander''s Arcanum'),
+(8,23318,'Lieutenant Commander''s Silk Cowl',546,'Lieutenant Commander''s Arcanum'),
+(8,23319,'Lieutenant Commander''s Silk Mantle',546,'Lieutenant Commander''s Arcanum');
+
+-- Paladin: Lieutenant Commander's Redoubt
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+
+(2, 23272, 'Knight-Captain''s Lamellar Breastplate', 544, 'Lieutenant Commander''s Redoubt'),
+(2, 23273, 'Knight-Captain''s Lamellar Leggings', 544, 'Lieutenant Commander''s Redoubt'),
+(2, 23274, 'Knight-Lieutenant''s Lamellar Gauntlets', 544, 'Lieutenant Commander''s Redoubt'),
+(2, 23275, 'Knight-Lieutenant''s Lamellar Sabatons', 544, 'Lieutenant Commander''s Redoubt'),
+(2, 23276, 'Lieutenant Commander''s Lamellar Headguard', 544, 'Lieutenant Commander''s Redoubt'),
+(2, 23277, 'Lieutenant Commander''s Lamellar Shoulders', 544, 'Lieutenant Commander''s Redoubt');
+
+-- Priest: Lieutenant Commander's Investiture
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+
+(5, 23303, 'Knight-Captain''s Satin Tunic', 549, 'Lieutenant Commander''s Investiture'),
+(5, 23302, 'Knight-Captain''s Satin Legguards', 549, 'Lieutenant Commander''s Investiture'),
+(5, 23288, 'Knight-Lieutenant''s Satin Handwraps', 549, 'Lieutenant Commander''s Investiture'),
+(5, 23289, 'Knight-Lieutenant''s Satin Walkers', 549, 'Lieutenant Commander''s Investiture'),
+(5, 23316, 'Lieutenant Commander''s Satin Hood', 549, 'Lieutenant Commander''s Investiture'),
+(5, 23317, 'Lieutenant Commander''s Satin Mantle', 549, 'Lieutenant Commander''s Investiture');
+
+
+-- Rogue: Lieutenant Commander's Guard
+INSERT INTO araxia_world.black_market_armor_sets
+(class, item_id, name, item_set_id, set_name)
+VALUES
+(4, 23298, 'Knight-Captain''s Leather Chestpiece', 548, 'Lieutenant Commander''s Guard'),
+(4, 23299, 'Knight-Captain''s Leather Legguards', 548, 'Lieutenant Commander''s Guard'),
+(4, 23284, 'Knight-Lieutenant''s Leather Grips', 548, 'Lieutenant Commander''s Guard'),
+(4, 23285, 'Knight-Lieutenant''s Leather Walkers', 548, 'Lieutenant Commander''s Guard'),
+(4, 23312, 'Lieutenant Commander''s Leather Helm', 548, 'Lieutenant Commander''s Guard'),
+(4, 23313, 'Lieutenant Commander''s Leather Shoulders', 548, 'Lieutenant Commander''s Guard');
+
+-- Shaman: Lieutenant Commander's Earthshaker
+INSERT INTO araxia_world.black_market_armor_sets
+(class, item_id, name, item_set_id, set_name)
+VALUES
+(7, 29599, 'Lieutenant Commander''s Mail Pauldrons', 718, 'Lieutenant Commander''s Earthshaker'),
+(7, 29595, 'Knight-Lieutenant''s Mail Vices', 718, 'Lieutenant Commander''s Earthshaker'),
+(7, 29597, 'Knight-Lieutenant''s Mail Legguards', 718, 'Lieutenant Commander''s Earthshaker'),
+(7, 29596, 'Knight-Captain''s Mail Hauberk', 718, 'Lieutenant Commander''s Earthshaker'),
+(7, 29598, 'Lieutenant Commander''s Mail Headguard', 718, 'Lieutenant Commander''s Earthshaker'),
+(7, 29594, 'Knight-Lieutenant''s Mail Greaves', 718, 'Lieutenant Commander''s Earthshaker');
+
+-- Warlock: Lieutenant Commander's Dreadgear
+INSERT INTO araxia_world.black_market_armor_sets
+(class, item_id, name, item_set_id, set_name)
+VALUES
+(9, 23296, 'Knight-Captain''s Dreadweave Legguards', 547, 'Lieutenant Commander''s Dreadgear'),
+(9, 23297, 'Knight-Captain''s Dreadweave Tunic', 547, 'Lieutenant Commander''s Dreadgear'),
+(9, 23282, 'Knight-Lieutenant''s Dreadweave Handwraps', 547, 'Lieutenant Commander''s Dreadgear'),
+(9, 23283, 'Knight-Lieutenant''s Dreadweave Walkers', 547, 'Lieutenant Commander''s Dreadgear'),
+(9, 23310, 'Lieutenant Commander''s Dreadweave Cowl', 547, 'Lieutenant Commander''s Dreadgear'),
+(9, 23311, 'Lieutenant Commander''s Dreadweave Spaulders', 547, 'Lieutenant Commander''s Dreadgear');
+
+-- Warrior: Lieutenant Commander's Battlearmor
+INSERT INTO araxia_world.black_market_armor_sets
+(class, item_id, name, item_set_id, set_name)
+VALUES
+(1, 23300, 'Knight-Captain''s Plate Hauberk', 545, 'Lieutenant Commander''s Battlearmor'),
+(1, 23301, 'Knight-Captain''s Plate Leggings', 545, 'Lieutenant Commander''s Battlearmor'),
+(1, 23286, 'Knight-Lieutenant''s Plate Gauntlets', 545, 'Lieutenant Commander''s Battlearmor'),
+(1, 23287, 'Knight-Lieutenant''s Plate Greaves', 545, 'Lieutenant Commander''s Battlearmor'),
+(1, 23314, 'Lieutenant Commander''s Plate Helmet', 545, 'Lieutenant Commander''s Battlearmor'),
+(1, 23315, 'Lieutenant Commander''s Plate Shoulders', 545, 'Lieutenant Commander''s Battlearmor');
+
+
+
+
+--[[Level 60 Low Tier Set ]]
+
+
+
+
+
+-- Druid: Champion's Refuge
+INSERT INTO araxia_world.black_market_armor_sets
+(class, item_id, name, item_set_id, set_name)
+VALUES
+(11, 22863, 'Blood Guard''s Dragonhide Grips', 539, 'Blood Guard''s Dragonhide'),
+(11, 22852, 'Blood Guard''s Dragonhide Treads', 539, 'Blood Guard''s Dragonhide'),
+(11, 22877, 'Legionnaire''s Dragonhide Chestpiece', 539, 'Blood Guard''s Dragonhide'),
+(11, 22878, 'Legionnaire''s Dragonhide Leggings', 539, 'Blood Guard''s Dragonhide'),
+(11, 23253, 'Champion''s Dragonhide Headguard', 539, 'Blood Guard''s Dragonhide'),
+(11, 23254, 'Champion''s Dragonhide Shoulders', 539, 'Blood Guard''s Dragonhide');
+
+
+--Hunter: Champion's Pursuance
+INSERT INTO araxia_world.black_market_armor_sets
+(class, item_id, name, item_set_id, set_name)
+VALUES
+(3, 22843, 'Blood Guard''s Chain Greaves', 543, 'Blood Guard''s Chain'),
+(3, 22862, 'Blood Guard''s Chain Vices', 543, 'Blood Guard''s Chain'),
+(3, 23251, 'Champion''s Chain Helm', 543, 'Blood Guard''s Chain'),
+(3, 23252, 'Champion''s Chain Shoulders', 543, 'Blood Guard''s Chain'),
+(3, 22874, 'Legionnaire''s Chain Hauberk', 543, 'Blood Guard''s Chain'),
+(3, 22875, 'Legionnaire''s Chain Legguards', 543, 'Blood Guard''s Chain');
+
+-- Insert Armor Sets for Mage: Champion's Arcanum Item set 542
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(8, 22870, 'Blood Guard''s Silk Handwraps', 542, 'Champion''s Arcanum'),
+(8, 22860, 'Blood Guard''s Silk Walkers', 542, 'Champion''s Arcanum'),
+(8, 23263, 'Champion''s Silk Cowl', 542, 'Champion''s Arcanum'),
+(8, 23264, 'Champion''s Silk Mantle', 542, 'Champion''s Arcanum'),
+(8, 22883, 'Legionnaire''s Silk Leggings', 542, 'Champion''s Arcanum'),
+(8, 22886, 'Legionnaire''s Silk Tunic', 542, 'Champion''s Arcanum');
+
+-- Insert Armor Sets for Paladin: Champion's Redoubt Item Set 697
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(2, 29600, 'Blood Guard''s Lamellar Gauntlets', 697, 'Champion''s Redoubt'),
+(2, 29601, 'Blood Guard''s Lamellar Sabatons', 697, 'Champion''s Redoubt'),
+(2, 29602, 'Legionnaire''s Lamellar Breastplate', 697, 'Champion''s Redoubt'),
+(2, 29603, 'Legionnaire''s Lamellar Leggings', 697, 'Champion''s Redoubt'),
+(2, 29604, 'Champion''s Lamellar Headguard', 697, 'Champion''s Redoubt'),
+(2, 29605, 'Champion''s Lamellar Shoulders', 697, 'Champion''s Redoubt');
+
+-- Insert Armor Sets for Priest: Champion's Investiture Item Set 540
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(5, 22869, 'Blood Guard''s Satin Handwraps', 540, 'Champion''s Investiture'),
+(5, 22859, 'Blood Guard''s Satin Walkers', 540, 'Champion''s Investiture'),
+(5, 22882, 'Legionnaire''s Satin Legguards', 540, 'Champion''s Investiture'),
+(5, 22885, 'Legionnaire''s Satin Tunic', 540, 'Champion''s Investiture'),
+(5, 23261, 'Champion''s Satin Hood', 540, 'Champion''s Investiture'),
+(5, 23262, 'Champion''s Satin Mantle', 540, 'Champion''s Investiture');
+
+-- Insert Armor Sets for Rogue: Champion's Guard Item Set 522
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(4, 22864, 'Blood Guard''s Leather Grips', 522, 'Champion''s Guard'),
+(4, 22856, 'Blood Guard''s Leather Walkers', 522, 'Champion''s Guard'),
+(4, 22879, 'Legionnaire''s Leather Chestpiece', 522, 'Champion''s Guard'),
+(4, 22880, 'Legionnaire''s Leather Legguards', 522, 'Champion''s Guard'),
+(4, 23257, 'Champion''s Leather Helm', 522, 'Champion''s Guard'),
+(4, 23258, 'Champion''s Leather Shoulders', 522, 'Champion''s Guard');
+
+-- Insert Armor Sets for Shaman: Champion's Stormcaller Item set 538
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(7, 22857, 'Blood Guard''s Mail Greaves', 538, 'Champion''s Stormcaller'),
+(7, 22867, 'Blood Guard''s Mail Vices', 538, 'Champion''s Stormcaller'),
+(7, 22876, 'Legionnaire''s Mail Hauberk', 538, 'Champion''s Stormcaller'),
+(7, 22887, 'Legionnaire''s Mail Leggings', 538, 'Champion''s Stormcaller'),
+(7, 23259, 'Champion''s Mail Headguard', 538, 'Champion''s Stormcaller'),
+(7, 23260, 'Champion''s Mail Pauldrons', 538, 'Champion''s Stormcaller');
+
+-- Warlock: Champion's Dreadgear Item Set 541
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(9, 22865, 'Blood Guard''s Dreadweave Handwraps', 541, 'Champion''s Dreadgear'),
+(9, 22855, 'Blood Guard''s Dreadweave Walkers', 541, 'Champion''s Dreadgear'),
+(9, 23255, 'Legionnaire''s Dreadweave Cowl', 541, 'Champion''s Dreadgear'),
+(9, 23256, 'Legionnaire''s Dreadweave Spaulders', 541, 'Champion''s Dreadgear'),
+(9, 22881, 'Champion''s Dreadweave Legguards', 541, 'Champion''s Dreadgear'),
+(9, 22884, 'Champion''s Dreadweave Tunic', 541, 'Champion''s Dreadgear');
+
+-- Warrior: Champion's Battlearmor Item Set 537
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(1, 22868, 'Blood Guard''s Plate Gauntlets', 537, 'Champion''s Battlearmor'),
+(1, 22858, 'Blood Guard''s Plate Greaves', 537, 'Champion''s Battlearmor'),
+(1, 22872, 'Legionnaire''s Plate Hauberk', 537, 'Champion''s Battlearmor'),
+(1, 22873, 'Legionnaire''s Plate Leggings', 537, 'Champion''s Battlearmor'),
+(1, 23244, 'Champion''s Plate Helm', 537, 'Champion''s Battlearmor'),
+(1, 23243, 'Champion''s Plate Shoulders', 537, 'Champion''s Battlearmor');
+
+
+
+
+
+--[[Level 60 High Tier Set ]]
+
+
+-- Insert Armor Sets for Druid: Field Marshal's Sanctuary Item Set 397
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(11, 16452, 'Field Marshal''s Dragonhide Breastplate', 397, 'Field Marshal''s Sanctuary'),
+(11, 16451, 'Field Marshal''s Dragonhide Helmet', 397, 'Field Marshal''s Sanctuary'),
+(11, 16449, 'Field Marshal''s Dragonhide Spaulders', 397, 'Field Marshal''s Sanctuary'),
+(11, 16459, 'Marshal''s Dragonhide Boots', 397, 'Field Marshal''s Sanctuary'),
+(11, 16448, 'Marshal''s Dragonhide Gauntlets', 397, 'Field Marshal''s Sanctuary'),
+(11, 16450, 'Marshal''s Dragonhide Legguards', 397, 'Field Marshal''s Sanctuary');
+
+-- Insert Armor Sets for Hunter: Field Marshal's Pursuit Item Set 395
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(3, 16466, 'Field Marshal''s Chain Breastplate', 395, 'Field Marshal''s Pursuit'),
+(3, 16465, 'Field Marshal''s Chain Helm', 395, 'Field Marshal''s Pursuit'),
+(3, 16468, 'Field Marshal''s Chain Spaulders', 395, 'Field Marshal''s Pursuit'),
+(3, 16462, 'Marshal''s Chain Boots', 395, 'Field Marshal''s Pursuit'),
+(3, 16463, 'Marshal''s Chain Grips', 395, 'Field Marshal''s Pursuit'),
+(3, 16467, 'Marshal''s Chain Legguards', 395, 'Field Marshal''s Pursuit');
+
+-- Insert Armor Sets for Mage: Field Marshal's Regalia Item set 388
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(8, 16441, 'Field Marshal''s Coronet', 388, 'Field Marshal''s Regalia'),
+(8, 16444, 'Field Marshal''s Silk Spaulders', 388, 'Field Marshal''s Regalia'),
+(8, 16443, 'Field Marshal''s Silk Vestments', 388, 'Field Marshal''s Regalia'),
+(8, 16437, 'Marshal''s Silk Footwraps', 388, 'Field Marshal''s Regalia'),
+(8, 16440, 'Marshal''s Silk Gloves', 388, 'Field Marshal''s Regalia'),
+(8, 16442, 'Marshal''s Silk Leggings', 388, 'Field Marshal''s Regalia');
+
+-- Insert Armor Sets for Paladin: Field Marshal's Aegis Item Set 402
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(2, 16473, 'Field Marshal''s Lamellar Chestplate', 402, 'Field Marshal''s Aegis'),
+(2, 16474, 'Field Marshal''s Lamellar Faceguard', 402, 'Field Marshal''s Aegis'),
+(2, 16476, 'Field Marshal''s Lamellar Pauldrons', 402, 'Field Marshal''s Aegis'),
+(2, 16472, 'Marshal''s Lamellar Boots', 402, 'Field Marshal''s Aegis'),
+(2, 16471, 'Marshal''s Lamellar Gloves', 402, 'Field Marshal''s Aegis'),
+(2, 16475, 'Marshal''s Lamellar Legplates', 402, 'Field Marshal''s Aegis');
+
+-- Insert Armor Sets for Priest: Field Marshal's Raiment Item Set 389
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(5, 17604, 'Field Marshal''s Satin Mantle', 389, 'Field Marshal''s Raiment'),
+(5, 17603, 'Marshal''s Satin Pants', 389, 'Field Marshal''s Raiment'),
+(5, 17605, 'Field Marshal''s Satin Vestments', 389, 'Field Marshal''s Raiment'),
+(5, 17608, 'Marshal''s Satin Gloves', 389, 'Field Marshal''s Raiment'),
+(5, 17607, 'Marshal''s Satin Sandals', 389, 'Field Marshal''s Raiment'),
+(5, 17602, 'Field Marshal''s Headdress', 389, 'Field Marshal''s Raiment');
+
+-- Insert Armor Sets for Rogue: Field Marshal's Vestments Item Set 394
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(4, 16453, 'Field Marshal''s Leather Chestpiece', 394, 'Field Marshal''s Vestments'),
+(4, 16457, 'Field Marshal''s Leather Epaulets', 394, 'Field Marshal''s Vestments'),
+(4, 16455, 'Field Marshal''s Leather Mask', 394, 'Field Marshal''s Vestments'),
+(4, 16446, 'Marshal''s Leather Footguards', 394, 'Field Marshal''s Vestments'),
+(4, 16454, 'Marshal''s Leather Handgrips', 394, 'Field Marshal''s Vestments'),
+(4, 16456, 'Marshal''s Leather Leggings', 394, 'Field Marshal''s Vestments');
+
+-- Insert Armor Sets for Shaman: Field Marshal's Earthshaker Item Set 717
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(7, 29608, 'Marshal''s Mail Leggings', 717, 'Field Marshal''s Earthshaker'),
+(7, 29606, 'Marshal''s Mail Boots', 717, 'Field Marshal''s Earthshaker'),
+(7, 29611, 'Field Marshal''s Mail Spaulders', 717, 'Field Marshal''s Earthshaker'),
+(7, 29609, 'Field Marshal''s Mail Armor', 717, 'Field Marshal''s Earthshaker'),
+(7, 29607, 'Marshal''s Mail Gauntlets', 717, 'Field Marshal''s Earthshaker'),
+(7, 29610, 'Field Marshal''s Mail Helm', 717, 'Field Marshal''s Earthshaker');
+
+-- Insert Armor Sets for Warlock: Field Marshal's Threads Item Set 392
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(9, 17581, 'Field Marshal''s Dreadweave Robe', 392, 'Field Marshal''s Threads'),
+(9, 17580, 'Field Marshal''s Dreadweave Shoulders', 392, 'Field Marshal''s Threads'),
+(9, 17583, 'Marshal''s Dreadweave Boots', 392, 'Field Marshal''s Threads'),
+(9, 17584, 'Marshal''s Dreadweave Gloves', 392, 'Field Marshal''s Threads'),
+(9, 17579, 'Marshal''s Dreadweave Leggings', 392, 'Field Marshal''s Threads'),
+(9, 17578, 'Field Marshal''s Coronal', 392, 'Field Marshal''s Threads');
+
+-- Insert Armor Sets for Warrior: Field Marshal's Battlegear Item Set 384
+INSERT INTO `black_market_armor_sets` (`class`, `item_id`, `name`, `item_set_id`, `set_name`) VALUES 
+(1, 16477, 'Field Marshal''s Plate Armor', 384, 'Field Marshal''s Battlegear'),
+(1, 16478, 'Field Marshal''s Plate Helm', 384, 'Field Marshal''s Battlegear'),
+(1, 16480, 'Field Marshal''s Plate Shoulderguards', 384, 'Field Marshal''s Battlegear'),
+(1, 16483, 'Marshal''s Plate Boots', 384, 'Field Marshal''s Battlegear'),
+(1, 16484, 'Marshal''s Plate Gauntlets', 384, 'Field Marshal''s Battlegear'),
+(1, 16479, 'Marshal''s Plate Legguards', 384, 'Field Marshal''s Battlegear');
